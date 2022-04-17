0b3e4d16566354e8427988bbf092fa56ff75cc1e
Andy Primawan <andy@primawan.com>
2022-04-17 15:14:46 +0700

    fix eslint warns

--- src/logic.js
+++ src/logic.js
@@ -1,66 +1,65 @@
 const moment = require('moment');
 
-const processSingleLine = (line, outputFormat) => {
-    // Check for NULL line
-    if (! line) return;
+function processSingleLine(line) {
+  const trx = JSON.parse(line);
 
-    const trx = JSON.parse(line);
+  const orderId = trx.order_id;
+  const orderDateTime = moment(trx.order_date).utc().format('YYYY-MM-DDThh:mm:ssZZ');
 
-    const order_id = trx.order_id;
-    const order_datetime = moment(trx.order_date).utc().format('YYYY-MM-DDThh:mm:ssZZ');
+  let totalOrderValuePreDiscount = 0;
+  let distinctUnitCount = 0;
+  let totalUnitsCount = 0;
 
-    let total_order_value_pre_discount = 0;
-    let distinct_unit_count = 0;
-    let total_units_count = 0;
+  trx.items.forEach((item) => {
+    totalOrderValuePreDiscount += item.quantity * item.unit_price;
+    distinctUnitCount += 1;
+    totalUnitsCount += item.quantity;
+  });
 
-    trx.items.forEach(item => {
-        total_order_value_pre_discount += item.quantity * item.unit_price;
-        distinct_unit_count++;
-        total_units_count += item.quantity;
-    });
-
-    // Calculate discount
-    // Assumption:
-    // 1. discount calculation is executed in order (priority #1, then priority #2, etc)
-    // 2. discount objects from the JSONL are stored in ascending order (priority #1, priority #2, etc)
-    let total_order_value = total_order_value_pre_discount;
-    trx.discounts.forEach(discount => {
-        if (discount.type === 'DOLLAR') {
-            total_order_value -= discount.value;
-        } else if (discount.type === 'PERCENTAGE') {
-            total_order_value -= total_order_value * discount.value / 100;
-        }
-    });
-
-    const average_unit_price = total_order_value / total_units_count;
-    
-    const customer_state = trx.customer.shipping_address.state;
-
-    const result = {
-        order_id,
-        order_datetime,
-        total_order_value,
-        average_unit_price,
-        distinct_unit_count,
-        total_units_count,
-        customer_state,
-    };
-
-    if (outputFormat && outputFormat == 'object') {
-        // To be called from unit-tests
-        return result;
-    } else {
-        // Return output as a CSV line
-        let values = [];
-
-        Object.values(result).forEach(value => {
-            values.push(value);
-        });
-        return Buffer.from(values.join(',') + "\n");
+  // Calculate discount
+  // Assumption:
+  // 1. discount calculation is executed in order (priority #1, then priority #2, etc)
+  // 2. discount objects from the JSONL are stored in ascending order
+  //    (priority #1, priority #2, etc)
+  let totalOrderValue = totalOrderValuePreDiscount;
+  trx.discounts.forEach((discount) => {
+    if (discount.type === 'DOLLAR') {
+      totalOrderValue -= discount.value;
+    } else if (discount.type === 'PERCENTAGE') {
+      totalOrderValue -= totalOrderValue * (discount.value / 100);
     }
+  });
+
+  const averageUnitPrice = totalOrderValue / totalUnitsCount;
+
+  const customerState = trx.customer.shipping_address.state;
+
+  const result = {
+    orderId,
+    orderDateTime,
+    totalOrderValue,
+    averageUnitPrice,
+    distinctUnitCount,
+    totalUnitsCount,
+    customerState,
+  };
+
+  return result;
 }
 
+function processSingleLineReturnBuffer(line) {
+  // Return output as a CSV line
+
+  const result = processSingleLine(line);
+  const values = [];
+
+  Object.values(result).forEach((value) => {
+    values.push(value);
+  });
+  return Buffer.from(`${values.join(',')}\n`);
+}
 
 module.exports = {
-    processSingleLine
+  processSingleLine,
+  processSingleLineReturnBuffer,
 };
--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const eventStream = require('event-stream');
-const { processSingleLine } = require('./logic')
+const { processSingleLine } = require('./logic');
 
-const s = fs
-    .createReadStream('./test.jsonl')
-    .pipe(eventStream.split())
-    .pipe(
-        eventStream
-            .mapSync(processSingleLine)
-            .on('error', err => {
-                console.error('Error while reading file: ', err);
-            })
-            .on('end', () => {
-                console.log('Done.');
-            })
-    )
-    .pipe(fs.createWriteStream('./output.csv'));
+fs
+  .createReadStream('./test.jsonl')
+  .pipe(eventStream.split())
+  .pipe(
+    eventStream
+      .mapSync(processSingleLine)
+      .on('error', (err) => {
+        console.error('Error while reading file: ', err); // eslint-disable-line no-console
+      })
+      .on('end', () => {
+        console.log('Done.'); // eslint-disable-line no-console
+      }),
+  )
+  .pipe(fs.createWriteStream('./output.csv'));
